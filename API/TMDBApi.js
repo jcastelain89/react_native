3ade77537874f4b8fadcbcfb847a08ab01d1bdd2
jcastelain89 <jcastelain@macbook-pro-de-wizards.home>
2018-11-23 00:48:22 +0100

    added images + loader + pagination

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,11 +1,15 @@
 // API/TMDBApi.js
 
-const API_TOKEN = "a41550321c0ad782e36e054d51c6a3b8";
+const API_TOKEN = 'a41550321c0ad782e36e054d51c6a3b8';
 
-export function getFilmsFromApiWithSearchedText (text) {
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + '&page=' + page
 
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.error(error))
+}
+
+export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
 }
--- Components/Search.js
+++ Components/Search.js
@@ -1,55 +1,120 @@
 // Components/Search.js
 
 import React from 'react'
-import { StyleSheet, View, Button, TextInput, FlatList, Text } from 'react-native'
+import { StyleSheet, View, Button, TextInput, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi' 
 
 class Search extends React.Component {
-    
     constructor(props) {
         super(props)
-        this.state = { films: [] }
+        this.state = {
+            films: [], // Initialisation de notre donnée films dans le state
+            isLoading: false // Par défaut à false car il n'y a pas de chargement tant qu'on ne lance pas de recherche
+        }
+        this.searchedText = ""  // Initialisation de notre donnée searchedText en dehors du state
+        this.page = 0 // Compteur pour connaître la page courante
+        this.totalPages = 0 // Nombre de pages totales pour savoir si on a atteint la fin des retours de l'API TMDB
     }
     
+    _searchTextInputChanged(text) {
+        this.searchedText = text
+    }
+
     _loadFilms() {
-        getFilmsFromApiWithSearchedText("roi").then(data => {
-            this.setState({ films: data.results })
+        console.log(this.searchedText) // Un log pour vérifier qu'on a bien le texte du TextInput
+        if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+            this.setState({ isLoading: true }) // Lancement du chargement
+            getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+                this.page = data.page
+                this.totalPages = data.total_pages
+                this.setState({ 
+                    films: [ ...this.state.films, ...data.results ], // same as films: this.state.films.concat(data.results)
+                    isLoading: false
+                })
+            })
+        }
+    }
+
+    _searchFilms() {  // Ici on va remettre à zéro les films de notre state
+        this.page = 0
+        this.totalPages = 0
+        this.setState({
+            films: [], 
+        }, () => { 
+            this._loadFilms() 
         })
     }
-    
-    render() {
-        return (
-            <View style={styles.main_container}>
-            <TextInput style={styles.textinput} placeholder='Titre du film'/>
-            <Button style={{ height: 50 }} title='Rechercher' onPress={() => this._loadFilms()}/>
-            <FlatList
-                data={this.state.films}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({item}) => <FilmItem film={item}/>}
-            />
-            </View>
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' />
+                </View>
             )
         }
     }
     
-    const styles = StyleSheet.create({
-        main_container: {
-            marginTop: 20,
-            flex: 1
-        },
-        textinput: {
-            marginLeft: 5, 
-            marginRight: 5, 
-            height: 50, 
-            borderColor: '#000000', 
-            borderWidth: 1, 
-            paddingLeft: 5
-        },
-        button: {
-            height: 50
-        }
-    })
+    render() {
+        console.log(this.state.isLoading)
+        return (
+            <View style={styles.main_container}>
+                <TextInput
+                    style={styles.textinput}
+                    placeholder='Titre du film'
+                    onChangeText={(text) => this._searchTextInputChanged(text)}
+                    onSubmitEditing={() => this._searchFilms()}
+                />
+                <Button 
+                    style={styles.button}
+                    title='Rechercher' 
+                    onPress={() => this._searchFilms()}
+                />
+                <FlatList
+                    data={this.state.films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <FilmItem film={item}/>}
+                    onEndReachedThreshold={0.5}
+                    onEndReached={() => {
+                        if (this.state.films.length > 0 && this.page < this.totalPages) { // On vérifie également qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
+                            this._loadFilms()
+                        }
+                    }}
+                />
+                {this._displayLoading()}
+            </View>
+        )
+    }
+}
     
-    export default Search
+const styles = StyleSheet.create({
+    main_container: {
+        marginTop: 20,
+        flex: 1
+    },
+    textinput: {
+        marginLeft: 5, 
+        marginRight: 5,
+        marginTop: 10, 
+        height: 50, 
+        borderColor: '#000000', 
+        borderWidth: 1, 
+        paddingLeft: 5
+    },
+    button: {
+        height: 50,
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
+
+export default Search
     
